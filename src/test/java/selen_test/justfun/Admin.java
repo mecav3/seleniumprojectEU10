dc01520c2003a153b3018d0d0248e1953f870982
memo <mecav3@gmail.com>
2023-02-09 23:15:38 +0300

    employee starts

---
+++ src/test/java/selen_test/justfun/Admin.java
@@ -0,0 +1,20 @@
+package selen_test.justfun;
+
+import org.testng.annotations.Test;
+
+public class Admin {
+    @Test
+    public void test0_get_user_list() {
+        
+    }
+
+    @Test
+    public void test1_login_to_users() {
+        
+    }
+
+    @Test
+    public void test2_verify_authority() {
+
+    }
+}
